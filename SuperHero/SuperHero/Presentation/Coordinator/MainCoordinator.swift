2d4de4d7c605116da290cacf853a757e3208eb37
StsVtr <99646875+StsVtr@users.noreply.github.com>
2022-02-16 18:43:23 +0200

    Add files via upload

---
+++ SuperHero/SuperHero/Presentation/Coordinator/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator.swift
+//  SuperHero
+//
+//  Created by Stanislav Vitriv on 16.02.2022.
+//
+
+import Foundation
---
+++ SuperHero/SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by Stanislav Vitriv on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
